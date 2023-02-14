be5845a0c4e007606947c32760eabd85f3e067f7
surex <samuser107@gmail.com>
2023-02-14 17:10:16 +0100

    fix large screen photos

--- components/SharedModal.tsx
+++ components/SharedModal.tsx
@@ -55,7 +55,7 @@ export default function SharedModal({
       }}
     >
       <div
-        className="relative z-50 flex aspect-[2/3] h-full max-w-7xl items-center wide:h-full xl:taller-than-854:h-auto"
+        className="relative z-50 flex aspect-[2/3] h-full max-w-7xl items-center wide:h-full"
         {...handlers}
       >
         {/* Main image */}
