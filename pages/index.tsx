c6416df11a7bceaa0f435e82975321e0ecfff935
surex <samuser107@gmail.com>
2023-02-15 09:35:24 +0100

    button contact

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,31 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useEffect, useRef } from 'react'
-import Bridge from '../components/Icons/Bridge'
-import Logo from '../components/Icons/Logo'
-import Modal from '../components/Modal'
-import cloudinary from '../utils/cloudinary'
-import getBase64ImageUrl from '../utils/generateBlurPlaceholder'
-import type { ImageProps } from '../utils/types'
-import { useLastViewedPhoto } from '../utils/useLastViewedPhoto'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+import Bridge from "../components/Icons/Bridge";
+import Logo from "../components/Icons/Logo";
+import Modal from "../components/Modal";
+import cloudinary from "../utils/cloudinary";
+import getBase64ImageUrl from "../utils/generateBlurPlaceholder";
+import type { ImageProps } from "../utils/types";
+import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
 
 const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
-  const router = useRouter()
-  const { photoId } = router.query
-  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto()
+  const router = useRouter();
+  const { photoId } = router.query;
+  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
 
-  const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null)
+  const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null);
 
   useEffect(() => {
     // This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
     if (lastViewedPhoto && !photoId) {
-      lastViewedPhotoRef.current.scrollIntoView({ block: 'center' })
-      setLastViewedPhoto(null)
+      lastViewedPhotoRef.current.scrollIntoView({ block: "center" });
+      setLastViewedPhoto(null);
     }
-  }, [photoId, lastViewedPhoto, setLastViewedPhoto])
+  }, [photoId, lastViewedPhoto, setLastViewedPhoto]);
 
   return (
     <>
@@ -45,7 +45,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           <Modal
             images={images}
             onClose={() => {
-              setLastViewedPhoto(photoId)
+              setLastViewedPhoto(photoId);
             }}
           />
         )}
@@ -53,7 +53,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
             <div className="absolute inset-0 flex items-center justify-center opacity-20">
               <span className=" h-full max-w-full items-center justify-center">
-                <Image src={'/me.jpg'} width='1000' height='1000'alt={''}/>
+                <Image src={"/me.jpg"} width="1000" height="1000" alt={""} />
               </span>
               <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
             </div>
@@ -66,14 +66,16 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
               Discover all photos taken by Louni Longheval
             </p>
-            <a
-              className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
-              href="https://instagram.com/8h_35m/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Contact
-            </a>
+            
+            <button className="relative overflow-hidden rounded-lg bg-black px-12 py-5 ring-red-500/50 ring-offset-black will-change-transform focus:outline-none focus:ring-1 focus:ring-offset-2">
+              <span className="absolute inset-px z-10 grid place-items-center rounded-lg bg-black from-neutral-800 text-white/75 font-semibold">
+                Contact
+              </span>
+              <span
+                aria-hidden
+                className="before:animate-disco before:bg-gradient-conic absolute inset-0 z-0 scale-x-[2.0] blur before:absolute before:inset-0 before:top-1/2 before:aspect-square before:from-purple-700 before:via-red-500 before:to-amber-400"
+              />
+            </button>
           </div>
           {images.map(({ id, public_id, format, blurDataUrl }) => (
             <Link
@@ -87,7 +89,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               <Image
                 alt="Next.js Conf photo"
                 className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                style={{ transform: 'translate3d(0, 0, 0)' }}
+                style={{ transform: "translate3d(0, 0, 0)" }}
                 placeholder="blur"
                 blurDataURL={blurDataUrl}
                 src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
@@ -103,27 +105,32 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         </div>
       </main>
       <footer className="p-6 text-center text-white/80 sm:p-12">
-        Made by {' '}
-        <span
-        >
-          Louni Longheval at <a href='https://techtoolsfactory.com' className='font-semibold hover:text-white transition duration-500'><b>TechToolsFactory</b></a>
+        Made by{" "}
+        <span>
+          Louni Longheval at{" "}
+          <a
+            href="https://techtoolsfactory.com"
+            className="font-semibold transition duration-500 hover:text-white"
+          >
+            <b>TechToolsFactory</b>
+          </a>
         </span>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 export async function getStaticProps() {
   const results = await cloudinary.v2.search
     .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-    .sort_by('public_id', 'desc')
+    .sort_by("public_id", "desc")
     .max_results(400)
-    .execute()
-  let reducedResults: ImageProps[] = []
+    .execute();
+  let reducedResults: ImageProps[] = [];
 
-  let i = 0
+  let i = 0;
   for (let result of results.resources) {
     reducedResults.push({
       id: i,
@@ -131,22 +138,22 @@ export async function getStaticProps() {
       width: result.width,
       public_id: result.public_id,
       format: result.format,
-    })
-    i++
+    });
+    i++;
   }
 
   const blurImagePromises = results.resources.map((image: ImageProps) => {
-    return getBase64ImageUrl(image)
-  })
-  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises)
+    return getBase64ImageUrl(image);
+  });
+  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
 
   for (let i = 0; i < reducedResults.length; i++) {
-    reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i]
+    reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
   }
 
   return {
     props: {
       images: reducedResults,
     },
-  }
+  };
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,18 @@ module.exports = {
         wide: { raw: '(min-aspect-ratio: 3 / 2)' },
         'taller-than-854': { raw: '(min-height: 854px)' },
       },
+      backgroundImage: {
+        'gradient-conic': 'conic-gradient(var(--tw-gradient-stops))',
+      },
+      keyframes: {
+        disco: {
+          '0%': { transform: 'translateY(-50%) rotate(0deg)' },
+          '100%': { transform: 'translateY(-50%) rotate(360deg)' },
+        },
+      },
+      animation: {
+        disco: 'disco 1.5s linear infinite',
+      },
     },
   },
   plugins: [],
