190cd467852f13c4913b7d2bd2ca4c275bd3cbb8
surex <samuser107@gmail.com>
2024-02-12 17:34:09 +0100

    Update images.tsx

--- pages/api/images.tsx
+++ pages/api/images.tsx
@@ -1,10 +1,10 @@
 // fetch all the images from the cloudinary database
-// import cloudinary from 'cloudinary';
+import cloudinary from '../../utils/cloudinary';
 export default async function handler(req, res) {
-  // const images = await cloudinary.v2.search
-  // .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-  // .sort_by("public_id", "asc")
-  // .max_results(400)
-  // .execute();
-  res.status(200).json({ text: 'Hello' });;
+  const images = await cloudinary.v2.search
+  .expression(`folder:libwater/*`)
+  .sort_by("public_id", "asc")
+  .max_results(400)
+  .execute();
+  res.status(200).json(images);;
 }
